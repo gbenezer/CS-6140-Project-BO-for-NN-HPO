c0fca89c6bc3ef7d9fe5a6676a8de4443ac6a571
Gil Benezer <gil.benezer@gmail.com>
2025-03-29 23:19:54 -0400

    removed a lot of unnecessary/obsolete code

--- test/rough_draft_testing.py
+++ test/rough_draft_testing.py
@@ -1,14 +1,6 @@
 # standard imports
 import torch.nn as nn
-import logging
 from multiprocessing import freeze_support
-import os
-from pathlib import Path
-from ax.global_stopping.strategies.improvement import ImprovementGlobalStoppingStrategy
-from ax.exceptions.core import OptimizationShouldStop
-import pandas as pd
-from ax.service.utils.report_utils import _pairwise_pareto_plotly_scatter
-import plotly.io as pio
 
 # acquisition functions
 from botorch.acquisition.analytic import (
@@ -46,9 +38,7 @@ import sys
 sys.path.insert(0, "C:/Users/Gil/Documents/Repositories/Python/CS_6140/Project")
 
 # # importing the correct package defined functions
-from src.network.create_network_lightning import create_ff_model
 from src.network.load_data import get_MNIST_data, get_Superconductivity_data
-from src.network.evaluate_network import evaluate_hyperparameters
 from src.Ax_BO.conduct_experiment import conduct_experiment
 import src.Ax_BO.experiment_definition as exp_def
 
@@ -56,13 +46,6 @@ import src.Ax_BO.experiment_definition as exp_def
 
 if __name__ == "__main__":
     freeze_support()
-
-    # remove sanity check and other extraneous stout printing
-    # logging.getLogger("lightning").setLevel(logging.WARNING)
-    # logging.getLogger("lightning.pytorch").setLevel(logging.WARNING)
-    # logging.getLogger("ax.modelbridge.dispatch_utils").setLevel(logging.WARNING)
-    # logging.getLogger("ax.service.utils.instantiation").setLevel(logging.WARNING)
-    # logging.getLogger("ax.service.ax_client").setLevel(logging.WARNING)
 
     # Data Loading
 
@@ -99,118 +82,6 @@ if __name__ == "__main__":
     )
 
     # Experiment code testing
-
-    # # random classification, single objective
-    # MNIST_experiment_df, MNIST_client_object, MNIST_experiment_object = (
-    #     conduct_experiment(
-    #         task="classification",
-    #         parameter_space=exp_def.MNIST_parameters,
-    #         objective=exp_def.MNIST_single_objective,
-    #         param_constraints=exp_def.p_constraints,
-    #         out_constraints=exp_def.o_constraints,
-    #         tracking_metrics=exp_def.classification_tracking_metrics_single,
-    #         acquisition_func_class=qLogNoisyExpectedImprovement,
-    #         train_loader=trainloader_MNIST,
-    #         valid_loader=validloader_MNIST,
-    #         test_loader=testloader_MNIST,
-    #         input_shape=(1, 28, 28),
-    #         number_input_features=784,
-    #         number_output_features=10,
-    #         loss=nn.CrossEntropyLoss(),
-    #         max_trials=100,
-    #         num_reps_per_trial=1,
-    #         max_epochs=5,
-    #         experiment_name="Random_MNIST_2",
-    #         global_early_stop=True,
-    #         fully_random=True,
-    #         interactive_plots=False,
-    #         seed=None,
-    #     )
-    # )
-
-    # # random regression, single objective
-    # Super_experiment_df, Super_client_object, Super_experiment_object = (
-    #     conduct_experiment(
-    #         task="regression",
-    #         parameter_space=exp_def.Superconductivity_parameters,
-    #         objective=exp_def.Superconductivity_single_objective,
-    #         param_constraints=exp_def.p_constraints,
-    #         out_constraints=exp_def.o_constraints,
-    #         tracking_metrics=exp_def.regression_tracking_metrics_single,
-    #         acquisition_func_class=qLogNoisyExpectedImprovement,
-    #         train_loader=trainloader_Super,
-    #         valid_loader=validloader_Super,
-    #         test_loader=testloader_Super,
-    #         input_shape=(1, 1, 81),
-    #         number_input_features=81,
-    #         number_output_features=1,
-    #         loss=nn.HuberLoss(),
-    #         max_trials=100,
-    #         num_reps_per_trial=1,
-    #         max_epochs=5,
-    #         experiment_name="Random_Super_1",
-    #         global_early_stop=True,
-    #         fully_random=True,
-    #         interactive_plots=False,
-    #         seed=None,
-    #     )
-    # )
-
-    # non-random classification, single objective
-    MNIST_experiment_df, MNIST_client_object, MNIST_experiment_object = (
-        conduct_experiment(
-            task="classification",
-            parameter_space=exp_def.MNIST_parameters,
-            objective=exp_def.MNIST_single_objective,
-            param_constraints=exp_def.p_constraints,
-            out_constraints=exp_def.o_constraints,
-            tracking_metrics=exp_def.classification_tracking_metrics_single,
-            acquisition_func_class=qLogNoisyExpectedImprovement,
-            train_loader=trainloader_MNIST,
-            valid_loader=validloader_MNIST,
-            test_loader=testloader_MNIST,
-            input_shape=(1, 28, 28),
-            number_input_features=784,
-            number_output_features=10,
-            loss=nn.CrossEntropyLoss(),
-            max_trials=50,
-            num_reps_per_trial=1,
-            max_epochs=5,
-            experiment_name="Random_MNIST_50_3",
-            global_early_stop=False,
-            fully_random=True,
-            interactive_plots=False,
-            seed=None,
-        )
-    )
-    
-    # non-random regression, single objective
-    Super_experiment_df, Super_client_object, Super_experiment_object = (
-        conduct_experiment(
-            task="regression",
-            parameter_space=exp_def.Superconductivity_parameters,
-            objective=exp_def.Superconductivity_single_objective,
-            param_constraints=exp_def.p_constraints,
-            out_constraints=exp_def.o_constraints,
-            tracking_metrics=exp_def.regression_tracking_metrics_single,
-            acquisition_func_class=qLogNoisyExpectedImprovement,
-            train_loader=trainloader_Super,
-            valid_loader=validloader_Super,
-            test_loader=testloader_Super,
-            input_shape=(1, 1, 81),
-            number_input_features=81,
-            number_output_features=1,
-            loss=nn.HuberLoss(),
-            max_trials=50,
-            num_reps_per_trial=1,
-            max_epochs=5,
-            experiment_name="Random_Super_50_3",
-            global_early_stop=False,
-            fully_random=True,
-            interactive_plots=False,
-            seed=None,
-        )
-    )
     
     # non-random classification, single objective
     MNIST_experiment_df, MNIST_client_object, MNIST_experiment_object = (
@@ -232,7 +103,7 @@ if __name__ == "__main__":
             max_trials=50,
             num_reps_per_trial=1,
             max_epochs=5,
-            experiment_name="qLogNEI_MNIST_2",
+            experiment_name="qLogNEI_MNIST_3",
             global_early_stop=False,
             fully_random=False,
             interactive_plots=True,
@@ -260,94 +131,10 @@ if __name__ == "__main__":
             max_trials=50,
             num_reps_per_trial=1,
             max_epochs=5,
-            experiment_name="qLogNEI_Super_2",
+            experiment_name="qLogNEI_Super_3",
             global_early_stop=False,
             fully_random=False,
             interactive_plots=True,
             seed=None,
         )
     )
-
-    # # non-random classification, multi-objective
-    # MNIST_experiment_df, MNIST_client_object, MNIST_experiment_object = (
-    #     conduct_experiment(
-    #         task="classification",
-    #         parameter_space=exp_def.MNIST_parameters,
-    #         objective=exp_def.MNIST_multiobjective,
-    #         param_constraints=exp_def.p_constraints,
-    #         out_constraints=exp_def.o_constraints,
-    #         tracking_metrics=exp_def.classification_tracking_metrics_multi,
-    #         acquisition_func_class=qLogNoisyExpectedHypervolumeImprovement,
-    #         train_loader=trainloader_MNIST,
-    #         valid_loader=validloader_MNIST,
-    #         test_loader=testloader_MNIST,
-    #         input_shape=(1, 28, 28),
-    #         number_input_features=784,
-    #         number_output_features=10,
-    #         loss=nn.CrossEntropyLoss(),
-    #         max_trials=100,
-    #         num_reps_per_trial=1,
-    #         max_epochs=5,
-    #         experiment_name="qLogNoisyExpectedHypervolumeImprovement_MNIST_1",
-    #         global_early_stop=False,
-    #         fully_random=False,
-    #         interactive_plots=False,
-    #         seed=None,
-    #     )
-    # )
-    
-    # # non-random classification, single objective
-    # MNIST_experiment_df, MNIST_client_object, MNIST_experiment_object = (
-    #     conduct_experiment(
-    #         task="classification",
-    #         parameter_space=exp_def.MNIST_parameters,
-    #         objective=exp_def.MNIST_single_objective,
-    #         param_constraints=exp_def.p_constraints,
-    #         out_constraints=exp_def.o_constraints,
-    #         tracking_metrics=exp_def.classification_tracking_metrics_single,
-    #         acquisition_func_class=qLogNoisyExpectedImprovement,
-    #         train_loader=trainloader_MNIST,
-    #         valid_loader=validloader_MNIST,
-    #         test_loader=testloader_MNIST,
-    #         input_shape=(1, 28, 28),
-    #         number_input_features=784,
-    #         number_output_features=10,
-    #         loss=nn.CrossEntropyLoss(),
-    #         max_trials=100,
-    #         num_reps_per_trial=1,
-    #         max_epochs=5,
-    #         experiment_name="qLogNoisyExpectedImprovement_MNIST_1",
-    #         global_early_stop=True,
-    #         fully_random=False,
-    #         interactive_plots=True,
-    #         seed=None,
-    #     )
-    # )
-
-    # # non-random regression, multi-objective
-    # Super_experiment_df, Super_client_object, Super_experiment_object = (
-    #     conduct_experiment(
-    #         task="regression",
-    #         parameter_space=exp_def.Superconductivity_parameters,
-    #         objective=exp_def.Superconductivity_multiobjective,
-    #         param_constraints=exp_def.p_constraints,
-    #         out_constraints=exp_def.o_constraints,
-    #         tracking_metrics=exp_def.regression_tracking_metrics_multi,
-    #         acquisition_func_class=qLogNoisyExpectedHypervolumeImprovement,
-    #         train_loader=trainloader_Super,
-    #         valid_loader=validloader_Super,
-    #         test_loader=testloader_Super,
-    #         input_shape=(1, 1, 81),
-    #         number_input_features=81,
-    #         number_output_features=1,
-    #         loss=nn.HuberLoss(),
-    #         max_trials=100,
-    #         num_reps_per_trial=1,
-    #         max_epochs=5,
-    #         experiment_name="qLogNoisyExpectedHypervolumeImprovement_Super_1",
-    #         global_early_stop=False,
-    #         fully_random=False,
-    #         interactive_plots=False,
-    #         seed=None,
-    #     )
-    # )
